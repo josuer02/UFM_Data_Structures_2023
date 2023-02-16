cbaefe8207583dfcb224e58208afb0710c87684d
josuer02 <josueruiz@ufm.edu>
2023-02-15 20:19:35 -0600

    Actividades de Queues (CircularQueue y Adicion de Search y peek

--- queues/test.py
+++ queues/test.py
@@ -1,38 +1,91 @@
-from queue import LinearQueue
+from queue import LinearQueue, CircularQueue
 
 
-# Queue instance
-queue = LinearQueue(5)
-print(queue)
 
-# Enqueues
-queue.enqueue('A')
-print(queue)
-queue.enqueue('B')
-print(queue)
-queue.enqueue('C')
-print(queue)
-queue.enqueue('D')
-print(queue)
-queue.enqueue('E')
-print(queue)
-queue.enqueue('F') # Queue Overflow
-queue.enqueue('G') # Queue Overflow
+# q=LinearQueue(5)
+# print(q)
 
-# Dequeues
-queue.dequeue()
-print(queue)
-queue.dequeue()
-print(queue)
-queue.dequeue()
-print(queue)
-queue.dequeue()
-print(queue)
-queue.dequeue()
-print(queue)
-queue.dequeue() # Queue Underflow (2nd scenario)
-queue.dequeue() # Queue Underflow (2nd scenario)
+# val = q.dequeue()
 
-queue_2 = LinearQueue(5)
-print(queue_2)
-queue.dequeue() # Queue Underflow (1st scenario)
+# q.enqueue('A')
+# print(q)
+# q.enqueue('B')
+# print(q)
+# q.enqueue('C')
+# print(q)
+
+# #busqueda
+# print('Searche A: ', q.search('A'))
+# print('Searche F: ', q.search('F'))
+# q.enqueue('D')
+# print(q)
+# q.enqueue('E')
+# print(q)
+# q.enqueue('EX')
+# print(q)
+
+
+# #Eliminar
+
+# val = q.dequeue()
+# print(q)
+# print('Element dequeued: {}'.format(val))
+
+# val = q.dequeue()
+# print(q)
+# print('Element dequeued: {}'.format(val))
+
+# val = q.dequeue()
+# print(q)
+# print('Element dequeued: {}'.format(val))
+
+# #peek
+# print('Element peeked: ', q.peek())
+
+# val = q.dequeue()
+# print(q)
+# print('Element dequeued: {}'.format(val))
+
+# val = q.dequeue()
+# print(q)
+# print('Element dequeued: {}'.format(val))
+
+# #Underflow 2 escenario
+# val = q.dequeue()
+
+##CircularQueue
+#Iniciamos la circularQueue
+w=CircularQueue(5)
+print(w)
+#llenamos la Queue
+w.enqueue('A')
+print(w)
+w.enqueue('B')
+print(w)
+w.enqueue('C')
+print(w)
+w.enqueue('D')
+print(w)
+w.enqueue('E')
+print(w)
+#Queue OverFlow
+w.enqueue('F')
+print(w)
+# Dequeue
+w.dequeue()
+print(w)
+w.dequeue()
+print(w)
+w.dequeue()
+print(w)
+w.dequeue()
+print(w)
+w.dequeue()
+print(w)
+#Underflow
+w.dequeue()
+print(w)
+#Search
+print('Searche E: ', w.search('E'))
+#Peek
+print('Element peeked: ', w.peek())
